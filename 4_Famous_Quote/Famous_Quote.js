913ff48d96985f0dbcf0271354788ddd827c6513
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-21 20:59:17 +0500

    Add files via upload

---
+++ 4_Famous_Quote/Famous_Quote.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log('Allama Iqbal, "Vision without power does bring moral elavation but cannot give a lasting culture"');
---
+++ 4_Famous_Quote/Famous_Quote.ts
@@ -0,0 +1,1 @@
+console.log('Allama Iqbal, "Vision without power does bring moral elavation but cannot give a lasting culture"');
