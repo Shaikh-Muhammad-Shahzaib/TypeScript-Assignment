5c9bde17f1bb4660f713ecb9484eff8162eb2153
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-21 21:02:29 +0500

    Add files via upload

---
+++ 5_Famous_Quote_2/Famous_Quote2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = 'Allama Iqbal';
+let message = '"Vision without power does bring moral elavation but cannot give a lasting culture"';
+console.log(`${famous_person} once Said, ${message}`);
---
+++ 5_Famous_Quote_2/Famous_Quote2.ts
@@ -0,0 +1,5 @@
+let famous_person :string = 'Allama Iqbal';
+
+let message :string = '"Vision without power does bring moral elavation but cannot give a lasting culture"';
+
+console.log(`${famous_person} once Said, ${message}`);
