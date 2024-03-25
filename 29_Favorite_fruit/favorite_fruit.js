e4ec7024b1c1832739978e1c990a6b9c472de590
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:52:05 +0500

    Add files via upload

---
+++ 29_Favorite_fruit/favorite_fruit.js
@@ -0,0 +1,18 @@
+"use strict";
+// favorite fruit array 
+let favoriteFruit = ['Apple', 'Orange', 'Mango', 'Banana', 'Peach'];
+if (favoriteFruit.includes('Apple')) {
+    console.log('I really like Apple!');
+}
+if (favoriteFruit.includes('Banana')) {
+    console.log('I really like Banana!');
+}
+if (favoriteFruit.includes('Orange')) {
+    console.log('I really like Orange!');
+}
+if (favoriteFruit.includes('Peach')) {
+    console.log('I really like Peach!');
+}
+if (favoriteFruit.includes('Mango')) {
+    console.log('I really like Mango!');
+}
---
+++ 29_Favorite_fruit/favorite_fruit.ts
@@ -0,0 +1,22 @@
+// favorite fruit array 
+let favoriteFruit :string[] = ['Apple', 'Orange', 'Mango', 'Banana', 'Peach'];
+if(favoriteFruit.includes('Apple')){
+    console.log('I really like Apple!');
+}
+
+if(favoriteFruit.includes('Banana')){
+    console.log('I really like Banana!');
+}
+
+if(favoriteFruit.includes('Orange')){
+    console.log('I really like Orange!');
+}
+
+if(favoriteFruit.includes('Peach')){
+    console.log('I really like Peach!');
+}
+
+if(favoriteFruit.includes('Mango')){
+    console.log('I really like Mango!');
+}
+
