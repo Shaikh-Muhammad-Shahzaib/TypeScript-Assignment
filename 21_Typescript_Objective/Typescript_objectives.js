f770cb200615ed642f8f546409d785cc24a968b4
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:45:33 +0500

    Add files via upload

---
+++ 21_Typescript_Objective/Typescript_objectives.js
@@ -0,0 +1,17 @@
+"use strict";
+// person object
+let person = {
+    Name: 'Shaikh Muhammad Shahzaib',
+    Age: 22,
+    Natuinality: 'Pakistan',
+    Student: true
+};
+console.log(person);
+// car object
+let car = {
+    Maker: 'Toyota',
+    Color: 'Wite',
+    Seating_Capacity: 4,
+    Automatic: true
+};
+console.log(car);
---
+++ 21_Typescript_Objective/Typescript_objectives.ts
@@ -0,0 +1,34 @@
+
+// write a program that create objects containing these items.
+// data type of person object
+interface person {
+    Name : String,
+    Age : Number,
+    Nationality : string,
+    Student : boolean
+}
+
+// person object
+let person  = {
+    Name : 'Shaikh Muhammad Shahzaib',
+    Age : 22,
+    Natuinality : 'Pakistan',
+    Student : true
+}
+console.log(person);
+
+// data type of car object
+interface car {
+    Maker : string,
+    color : String,
+    Seating_Capacity :number,
+    Automatic : boolean
+}
+// car object
+let car = {
+    Maker : 'Toyota',
+    Color : 'Wite',
+    Seating_Capacity : 4,
+    Automatic : true
+}
+console.log(car);
