55c20c04fb5d0b512fdca30f0c18f502a655b020
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:57:21 +0500

    Add files via upload

---
+++ 36_T_Shirts/t-shirts.js
@@ -0,0 +1,9 @@
+"use strict";
+// make a function
+function make_shirt(size, text) {
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+// call the function
+make_shirt('Large', 'Game On Hay');
+make_shirt('Medium', 'Game On Hay');
+make_shirt('Small', 'Game On Hay');
---
+++ 36_T_Shirts/t-shirts.ts
@@ -0,0 +1,10 @@
+// make a function
+
+function make_shirt(size:string, text:string){
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+
+// call the function
+make_shirt('Large', 'Game On Hay');
+make_shirt('Medium', 'Game On Hay');
+make_shirt('Small', 'Game On Hay');
