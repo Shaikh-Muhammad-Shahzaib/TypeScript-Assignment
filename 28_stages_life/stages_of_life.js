941b3f5d3f3a57cf874e69112f9b0054d9a3c29b
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:51:15 +0500

    Add files via upload

---
+++ 28_stages_life/stages_of_life.js
@@ -0,0 +1,127 @@
+"use strict";
+// Write an if-else chain that determine a person stage of life. 
+// set a value for the variable age and then
+//if age is 2
+let Age = 1;
+// if the perwon is less then 2 years old print a message that the person is a baby
+// if the perwon is less then 4 years old print a message that the person is a toddler
+// if the perwon is less then 13 years old print a message that the person is a Kid.
+// if the perwon is less then 20 years old print a message that the person is a Teenager.
+// if the perwon is less then 65 years old print a message that the person is an Adult.
+// if the person age is 65 or older print a message that the person is elder.
+if (Age < 2) {
+    console.log("Person is a baby");
+}
+else if (Age >= 2 && Age < 4) {
+    console.log("Person is a toddler");
+}
+else if (Age >= 4 && Age < 13) {
+    console.log("Person is a kid");
+}
+else if (Age >= 13 && Age < 20) {
+    console.log("Person is a teenager");
+}
+else if (Age >= 20 && Age < 65) {
+    console.log("Person is an adult");
+}
+else {
+    console.log("Person is an elder");
+}
+// if Age is 3
+Age = 3;
+if (Age < 2) {
+    console.log("Person is a baby");
+}
+else if (Age >= 2 && Age < 4) {
+    console.log("Person is a toddler");
+}
+else if (Age >= 4 && Age < 13) {
+    console.log("Person is a kid");
+}
+else if (Age >= 13 && Age < 20) {
+    console.log("Person is a teenager");
+}
+else if (Age >= 20 && Age < 65) {
+    console.log("Person is an adult");
+}
+else {
+    console.log("Person is an elder");
+}
+// if Age is 6
+Age = 6;
+if (Age < 2) {
+    console.log("Person is a baby");
+}
+else if (Age >= 2 && Age < 4) {
+    console.log("Person is a toddler");
+}
+else if (Age >= 4 && Age < 13) {
+    console.log("Person is a kid");
+}
+else if (Age >= 13 && Age < 20) {
+    console.log("Person is a teenager");
+}
+else if (Age >= 20 && Age < 65) {
+    console.log("Person is an adult");
+}
+else {
+    console.log("Person is an elder");
+}
+// if Age is 16
+Age = 16;
+if (Age < 2) {
+    console.log("Person is a baby");
+}
+else if (Age >= 2 && Age < 4) {
+    console.log("Person is a toddler");
+}
+else if (Age >= 4 && Age < 13) {
+    console.log("Person is a kid");
+}
+else if (Age >= 13 && Age < 20) {
+    console.log("Person is a teenager");
+}
+else if (Age >= 20 && Age < 65) {
+    console.log("Person is an adult");
+}
+else {
+    console.log("Person is an elder");
+} // if Age is 26
+Age = 26;
+if (Age < 2) {
+    console.log("Person is a baby");
+}
+else if (Age >= 2 && Age < 4) {
+    console.log("Person is a toddler");
+}
+else if (Age >= 4 && Age < 13) {
+    console.log("Person is a kid");
+}
+else if (Age >= 13 && Age < 20) {
+    console.log("Person is a teenager");
+}
+else if (Age >= 20 && Age < 65) {
+    console.log("Person is an adult");
+}
+else {
+    console.log("Person is an elder");
+} // if Age is 66
+Age = 66;
+if (Age < 2) {
+    console.log("Person is a baby");
+}
+else if (Age >= 2 && Age < 4) {
+    console.log("Person is a toddler");
+}
+else if (Age >= 4 && Age < 13) {
+    console.log("Person is a kid");
+}
+else if (Age >= 13 && Age < 20) {
+    console.log("Person is a teenager");
+}
+else if (Age >= 20 && Age < 65) {
+    console.log("Person is an adult");
+}
+else {
+    console.log("Person is an elder");
+}
