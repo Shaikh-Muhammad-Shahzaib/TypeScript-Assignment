0f8c9c7d414d08a6c1d3c1c66494662eeda269b7
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:38:23 +0500

    Add files via upload

---
+++ 14_Guest_List/Guest_list.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_list = ['Sami Hussain', 'Usama Ahmed', 'Taha Aftab', 'Ali Rasheed', 'Hamza Iqbal'];
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\n It is our pleasure to invite you in our party.\n\n Thank You!');
+}
---
+++ 14_Guest_List/Guest_list.ts
@@ -0,0 +1,6 @@
+let Guest_list :string[]= ['Sami Hussain','Usama Ahmed','Taha Aftab','Ali Rasheed','Hamza Iqbal'];
+
+for(let i=0; i<Guest_list.length; i++){
+    console.log('Dear Mr. ' + Guest_list[i] + ',\n\n It is our pleasure to invite you in our party.\n\n Thank You!');
+
+}
