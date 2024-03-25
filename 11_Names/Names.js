9542c5972786ba1b19a6d9fff6622f2634526d01
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:35:02 +0500

    Add files via upload

---
+++ 11_Names/Names.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Shahzaib', 'Hamza', 'Taha', 'Usama', 'Sami'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11_Names/Names.ts
@@ -0,0 +1,8 @@
+
+
+let names :string[]= ['Shahzaib','Hamza','Taha','Usama','Sami'];
+
+for(let i=0; i<names.length; i++){
+    console.log(names[i]);
+
+}
