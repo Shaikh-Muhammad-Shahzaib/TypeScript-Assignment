d75db593a602c0c7860881203bfc6ec64dc8e7e6
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:59:58 +0500

    Add files via upload

---
+++ 41_Magicians/magicians.js
@@ -0,0 +1,8 @@
+"use strict";
+let magician = ['Harry Pottor', 'Hermione Granger', 'Ron Weasley', 'Albus Dumbledore'];
+function show_magicians(magicians) {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magician);
