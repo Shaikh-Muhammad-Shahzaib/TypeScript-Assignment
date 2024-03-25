88491d7ffb579ee0ab3f02474ffdfe0a6beb8037
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:50:13 +0500

    Add files via upload

---
+++ 27_Alien_color_3/alien_color_3.ts
@@ -0,0 +1,66 @@
+// write three version of this program making sure each message is printed for the apprpriate color alien.
+
+// version 1 of the program
+let alienColor: string = "Green";
+
+//if the alien color is green print a message that player just earned 5 points.
+//if the alien color is yellow print a message that player just earned 10 points.
+//if the alien color is red print a message that player just earned 15 points.
+
+if (alienColor === "Green"){
+    console.log("Version 1: Player just earned 5 points.");
+}
+else if(alienColor === "Yellow"){
+    console.log("Player just earned 10 points.");
+}
+else if(alienColor === "Red"){
+    console.log("Player just earned 15 points.")
+}
+else{
+    console.log("Please select right color");
+} 
+// version 2 of the program
+alienColor = "Yellow"
+
+if (alienColor === "Green"){
+    console.log("Player just earned 5 points.");
+}
+else if(alienColor === "Yellow"){
+    console.log("Version 2: Player just earned 10 points.");
+}
+else if(alienColor === "Red"){
+    console.log("Player just earned 15 points.")
+}
+else{
+    console.log("Please select right color");
+} 
+// version 3 of the program
+alienColor = "Red"
+
+if (alienColor === "Green"){
+    console.log("Player just earned 5 points.");
+}
+else if(alienColor === "Yellow"){
+    console.log("Player just earned 10 points.");
+}
+else if(alienColor === "Red"){
+    console.log("Version 3: Player just earned 15 points.")
+}
+else{
+    console.log("Please select right color");
+} 
+// version 4 of the program
+alienColor = "Blue"
+
+if (alienColor === "Green"){
+    console.log("Player just earned 5 points.");
+}
+else if(alienColor === "Yellow"){
+    console.log("Player just earned 10 points.");
+}
+else if(alienColor === "Red"){
+    console.log("Player just earned 15 points.")
+}
+else{
+    console.log("Please select right color");
+} 
