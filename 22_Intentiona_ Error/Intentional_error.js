9f12dbf15613d43758a8b4726782770b402b9224
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:46:04 +0500

    Add files via upload

---
+++ 22_Intentiona_ Error/Intentional_error.js
@@ -0,0 +1,7 @@
+"use strict";
+// making a array 
+let newArray = ['Taha', 'Ali', 'Sami'];
+// making intention error
+console.log(newArray[4]);
+//correcting the error
+console.log(newArray);
---
+++ 22_Intentiona_ Error/Intentional_error.ts
@@ -0,0 +1,9 @@
+
+// making a array 
+let newArray :string[] = ['Taha', 'Ali', 'Sami'];
+
+// making intention error
+console.log(newArray[4]);
+
+//correcting the error
+console.log(newArray);
