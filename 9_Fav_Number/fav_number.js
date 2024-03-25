396046bef5549972880da2d83fd8bf99cd735c6d
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:33:02 +0500

    Add files via upload

---
+++ 9_Fav_Number/fav_number.js
@@ -0,0 +1,4 @@
+"use strict";
+const fav_number = 6;
+const message = 'Here is your favorite number ' + fav_number;
+console.log(message);
---
+++ 9_Fav_Number/fav_number.ts
@@ -0,0 +1,7 @@
+
+
+const fav_number :number = 6 ;
+
+const message :string = 'Here is your favorite number ' + fav_number ;
+
+console.log(message);
