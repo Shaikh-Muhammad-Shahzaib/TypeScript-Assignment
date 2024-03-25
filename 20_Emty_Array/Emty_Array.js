4b5d2dcd60f2170f01ede00d1f42d2e223a7f400
Shaikh-Muhammad-Shahzaib <161228092+Shaikh-Muhammad-Shahzaib@users.noreply.github.com>
2024-03-25 16:44:46 +0500

    Add files via upload

---
+++ 20_Emty_Array/Emty_Array.js
@@ -0,0 +1,10 @@
+"use strict";
+// Think of something you store in a array.
+let item = ['Pakistan', 'Karachi', 'Urdu'];
+console.log(item);
+// write a program that creates a list containing these items.
+let item2 = [];
+item2.push('Londone');
+item2.push('United Kindom');
+item2.push('America');
+console.log(item2);
---
+++ 20_Emty_Array/Emty_Array.ts
@@ -0,0 +1,16 @@
+
+// Think of something you store in a array.
+
+let item :string[] = ['Pakistan', 'Karachi','Urdu'];
+
+console.log(item);
+
+// write a program that creates a list containing these items.
+ let item2 :string[] = [] ;
+
+ item2.push('Londone');
+ item2.push('United Kindom');
+ item2.push('America');
+
+ console.log(item2);
+ 
